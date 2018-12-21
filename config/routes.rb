ba67d92f108dd9e74a421cef0f52b52cfa7944ac
= <iit2015038@iiita.ac.in>
2018-12-21 22:58:51 +0530

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
+  root 'blog#my_page'
   get 'pages/projects'
   get 'index/about'
   get 'index/contact'
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations' }
   devise_for :models
   get 'blog/my_page'
-  root 'blog#my_page'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_052517) do
+ActiveRecord::Schema.define(version: 2018_12_21_162512) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_12_18_052517) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
